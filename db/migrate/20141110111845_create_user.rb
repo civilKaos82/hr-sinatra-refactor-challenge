1722e4e35591fd5545fa514a0b8d339dd9388bd9
Mark Siemers <mark.siemers@gmail.com>
2017-04-09 17:47:26 -0700

    Upgrade to ActiveRecord 5 - Update migrations

--- db/migrate/20141110111845_create_user.rb
+++ db/migrate/20141110111845_create_user.rb
@@ -1,4 +1,4 @@
-class CreateUser < ActiveRecord::Migration
+class CreateUser < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
       t.string :name
--- db/migrate/20141110112419_create_salaries.rb
+++ db/migrate/20141110112419_create_salaries.rb
@@ -1,4 +1,4 @@
-class CreateSalaries < ActiveRecord::Migration
+class CreateSalaries < ActiveRecord::Migration[5.0]
   def change
     create_table :salaries do |t|
       t.integer :annual_salary
